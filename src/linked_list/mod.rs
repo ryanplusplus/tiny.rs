b95308932b8c160e44f31e46f31e1a30640208c1
Ryan Hartlage <ryanplusplus@gmail.com>
2020-06-29 19:24:33 -0400

    LinkedList no longer uses unsafe

--- src/linked_list/mod.rs
+++ src/linked_list/mod.rs
@@ -1,7 +1,7 @@
-use core::cell::UnsafeCell;
+use core::cell::Cell;
 
 pub struct LinkedListNode<'a, T> {
-    next: UnsafeCell<Option<&'a LinkedListNode<'a, T>>>,
+    next: Cell<Option<&'a LinkedListNode<'a, T>>>,
     value: T,
 }
 
@@ -12,7 +12,7 @@ pub struct LinkedList<'a, T> {
 impl<T> LinkedListNode<'_, T> {
     pub const fn new(value: T) -> Self {
         Self {
-            next: UnsafeCell::new(None),
+            next: Cell::new(None),
             value,
         }
     }
@@ -30,25 +30,25 @@ impl<'a, T> LinkedList<'a, T> {
     pub fn push_front(&mut self, node: &'a LinkedListNode<'a, T>) {
         match self.head {
             None => self.head = Some(node),
-            Some(head) => unsafe {
-                *node.next.get() = Some(head);
+            Some(head) => {
+                node.next.replace(Some(head));
                 self.head = Some(node);
-            },
+            }
         };
     }
 
     pub fn push_back(&mut self, node: &'a LinkedListNode<'a, T>) {
         match self.head {
             None => self.head = Some(node),
-            Some(head) => unsafe {
+            Some(head) => {
                 let mut current = Some(head);
 
-                while (*(&current.unwrap().next).get()).is_some() {
-                    current = *(&current.unwrap().next).get();
+                while (current.unwrap().next).get().is_some() {
+                    current = (current.unwrap().next).get();
                 }
 
-                *current.unwrap().next.get() = Some(node);
-            },
+                current.unwrap().next.replace(Some(node));
+            }
         };
     }
 
@@ -57,9 +57,9 @@ impl<'a, T> LinkedList<'a, T> {
 
         match self.head {
             None => (),
-            Some(head) => unsafe {
-                self.head = *head.next.get();
-            },
+            Some(head) => {
+                self.head = head.next.get();
+            }
         };
 
         popped
@@ -68,48 +68,48 @@ impl<'a, T> LinkedList<'a, T> {
     pub fn pop_back(&mut self) -> Option<&'a LinkedListNode<'a, T>> {
         match self.head {
             None => None,
-            Some(head) => unsafe {
-                if (*head.next.get()).is_some() {
+            Some(head) => {
+                if head.next.get().is_some() {
                     let mut previous = None;
                     let mut current = Some(head);
 
-                    while (*(&current.unwrap().next).get()).is_some() {
+                    while current.unwrap().next.get().is_some() {
                         previous = current;
-                        current = *(&current.unwrap().next).get();
+                        current = current.unwrap().next.get();
                     }
 
-                    *previous.unwrap().next.get() = None;
+                    previous.unwrap().next.replace(None);
                     current
                 } else {
                     let popped = self.head;
                     self.head = None;
                     popped
                 }
-            },
+            }
         }
     }
 
     pub fn count(&self) -> usize {
         match self.head {
             None => 0,
-            Some(head) => unsafe {
+            Some(head) => {
                 let mut current = Some(head);
                 let mut count = 1;
 
-                while (*(&current.unwrap().next).get()).is_some() {
+                while current.unwrap().next.get().is_some() {
                     count += 1;
-                    current = *(&current.unwrap().next).get();
+                    current = current.unwrap().next.get();
                 }
 
                 count
-            },
+            }
         }
     }
 
     pub fn for_each<F: FnMut(&T) -> bool>(&self, mut f: F) {
         match self.head {
             None => return,
-            Some(head) => unsafe {
+            Some(head) => {
                 let mut current = Some(head);
 
                 loop {
@@ -117,13 +117,13 @@ impl<'a, T> LinkedList<'a, T> {
                         break;
                     };
 
-                    if (*(&current.unwrap().next).get()).is_none() {
+                    if current.unwrap().next.get().is_none() {
                         break;
                     }
 
-                    current = *(&current.unwrap().next).get();
+                    current = current.unwrap().next.get();
                 }
-            },
+            }
         }
     }
 }
