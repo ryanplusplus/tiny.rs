83db13789ca57d964b4807e009d2f1708068821d
Ryan Hartlage <ryanplusplus@gmail.com>
2020-06-29 19:44:37 -0400

    It's working! It's working!

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -1,29 +1,43 @@
-extern crate alloc;
-use alloc::vec;
-use alloc::vec::Vec;
+use super::linked_list::LinkedList;
+use super::linked_list::LinkedListNode;
 use core::marker::PhantomData;
 
-pub struct Event<T, F: Fn(&T)> {
-    subscribers: Vec<F>,
+pub struct EventSubscription<T, F: Fn(&T)> {
+    callback: F,
     phantom: PhantomData<T>,
 }
 
-impl<T, F: Fn(&T)> Event<T, F> {
+impl<T, F: Fn(&T)> EventSubscription<T, F> {
+    pub fn new(callback: F) -> Self {
+        Self {
+            callback,
+            phantom: PhantomData,
+        }
+    }
+}
+
+pub struct Event<'a, T, F: Fn(&T)> {
+    subscribers: LinkedList<'a, EventSubscription<T, F>>,
+    phantom: PhantomData<T>,
+}
+
+impl<'a, T, F: Fn(&T)> Event<'a, T, F> {
     pub fn new() -> Self {
         Self {
-            subscribers: vec![],
+            subscribers: LinkedList::new(),
             phantom: PhantomData,
         }
     }
 
-    pub fn subscribe(&mut self, f: F) {
-        self.subscribers.push(f);
+    pub fn subscribe(&mut self, subscription: &'a LinkedListNode<'a, EventSubscription<T, F>>) {
+        self.subscribers.push_front(subscription);
     }
 
     pub fn publish(&mut self, t: &T) {
-        for subscriber in &mut self.subscribers {
-            subscriber(t);
-        }
+        self.subscribers.for_each(|subscriber| {
+            (subscriber.callback)(t);
+            true
+        });
     }
 }
 
--- src/event/test.rs
+++ src/event/test.rs
@@ -1,15 +1,21 @@
 use super::Event;
+use super::EventSubscription;
+use super::LinkedListNode;
 use core::cell::Cell;
 
 #[test]
 fn should() {
     let sum: Cell<u8> = Cell::new(0);
 
-    let mut event: Event<u8, _> = Event::new();
-
-    event.subscribe(|x| {
+    let sub = EventSubscription::new(|x| {
         sum.replace(sum.get() + *x);
     });
+
+    let subll = LinkedListNode::new(sub);
+
+    let mut event: Event<u8, _> = Event::new();
+
+    event.subscribe(&subll);
 
     event.publish(&3);
     event.publish(&4);
