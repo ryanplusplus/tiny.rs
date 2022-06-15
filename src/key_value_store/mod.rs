da2e95bf949c44df617a782c021d1be48fde020d
Ryan Hartlage <ryanplusplus@gmail.com>
2022-06-15 07:02:21 -0400

    Use Victor's macro for trivial Storable impls; provide more Storable impls

--- src/key_value_store/mod.rs
+++ src/key_value_store/mod.rs
@@ -8,70 +8,44 @@ pub trait KeyValueStore<'a> {
     fn on_change(&self) -> &Event<'a, Key>;
 }
 
-pub trait Storable {
-    fn can_deserialize_from(&self, bytes: &[Cell<u8>]) -> bool;
-    fn size(&self) -> u8;
-}
-
-impl Storable for u8 {
-    fn can_deserialize_from(&self, _bytes: &[Cell<u8>]) -> bool {
-        true
-    }
-
-    fn size(&self) -> u8 {
-        1
-    }
-}
-
-impl Storable for u16 {
-    fn can_deserialize_from(&self, _bytes: &[Cell<u8>]) -> bool {
-        true
-    }
-
-    fn size(&self) -> u8 {
-        2
-    }
-}
-
-impl Storable for u32 {
-    fn can_deserialize_from(&self, _bytes: &[Cell<u8>]) -> bool {
-        true
-    }
-
-    fn size(&self) -> u8 {
-        4
-    }
-}
-
-impl Storable for i8 {
-    fn can_deserialize_from(&self, _bytes: &[Cell<u8>]) -> bool {
-        true
-    }
-
-    fn size(&self) -> u8 {
-        1
-    }
-}
-
-impl Storable for i16 {
-    fn can_deserialize_from(&self, _bytes: &[Cell<u8>]) -> bool {
-        true
-    }
-
-    fn size(&self) -> u8 {
-        2
-    }
-}
-
-impl Storable for i32 {
-    fn can_deserialize_from(&self, _bytes: &[Cell<u8>]) -> bool {
-        true
-    }
-
-    fn size(&self) -> u8 {
-        4
-    }
-}
-
 pub type Key = u16;
 pub type Size = u8;
+
+pub trait Storable {
+    fn can_deserialize_from(&self, bytes: &[Cell<u8>]) -> bool;
+    fn size(&self) -> u8 {
+        core::mem::size_of_val(self) as u8
+    }
+}
+
+#[doc(hidden)]
+macro_rules! __impl_storable {
+    ($typ:ty) => {
+        impl Storable for $typ {
+            fn can_deserialize_from(&self, _: &[Cell<u8>]) -> bool {
+                true
+            }
+        }
+    };
+}
+
+__impl_storable!(u8);
+__impl_storable!(u16);
+__impl_storable!(u32);
+__impl_storable!(u64);
+__impl_storable!(u128);
+
+__impl_storable!(i8);
+__impl_storable!(i16);
+__impl_storable!(i32);
+__impl_storable!(i64);
+__impl_storable!(i128);
+
+__impl_storable!(usize);
+__impl_storable!(isize);
+
+impl Storable for bool {
+    fn can_deserialize_from(&self, bytes: &[Cell<u8>]) -> bool {
+        bytes[0].get() == (false as u8) || bytes[0].get() == (true as u8)
+    }
+}
